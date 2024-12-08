7fdbcd30013e5624e0307770434556121319b001
seeyoujeong <seeyoujeong.dev@gmail.com>
2024-12-08 13:57:40 +0900

    chore: url 한글 인식 못하는 오류 테스트

--- src/utils/common-utils.ts
+++ src/utils/common-utils.ts
@@ -1,19 +1,17 @@
-import { slug } from 'github-slugger';
-
 export function slugify(input?: string) {
   if (!input) return '';
 
   // make lower case and trim
-  // let slug = input.toLowerCase().trim();
+  let slug = input.toLowerCase().trim();
 
-  // // remove accents from charaters
-  // slug = slug.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  // remove accents from charaters
+  slug = slug.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
 
-  // // replace invalid chars with spaces
-  // //   slug = slug.replace(/[^a-z0-9\s-]/g, ' ').trim();
+  // replace invalid chars with spaces
+  //   slug = slug.replace(/[^a-z0-9\s-]/g, ' ').trim();
 
-  // // replace multiple spaces or hyphens with a single hyphen
-  // slug = slug.replace(/[\s-]+/g, '-');
+  // replace multiple spaces or hyphens with a single hyphen
+  slug = slug.replace(/[\s-]+/g, '-');
 
-  return slug(input);
+  return slug;
 }
